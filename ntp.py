9dfe9f60a67ba7b3ca8c737e20fc3a5fdd37023e
r3alityc0d3r <bill.t.ward@twc.com>
2016-02-07 23:00:45 -0600

    fixed bugs

--- ntp.py
+++ ntp.py
@@ -3,6 +3,14 @@ pyisac-ntp class
 
 Class to manage NTP on servers
 """
+from Pyisac.core import Core, Database, Package
 
 class Ntp(object):
-    pass
+    
+    def __init__(self,node):
+        os_type = "Ubuntu"
+        if os_type == "Ubuntu":
+            self.package_name = "ntp"
+        ntp_package = Package("ntp", node, "install")                                                                                
+        if not ntp_package.current():
+            print "need to install " + self.package_name
